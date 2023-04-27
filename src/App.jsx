cb0441b334f0a5c762d3560cfee44fba5ae16e79
amitkushwahatech <amitkushwahatech@gmail.com>
2023-04-27 07:55:42 +0530

    first commit21

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-import Timeline from "./components/timeline";
+import Timeline from "./components/Timeline";
 
 const App = () => {
   return (
